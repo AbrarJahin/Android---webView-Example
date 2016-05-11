a3b140be28322c1510bcea15ca2e9ff3f3c58634
S. M. Abrar Jahin <abrarjahin@live.com>
2016-05-11 13:26:57 +0600

    Init done

---
+++ app/src/test/java/com/appnucleus/abrarjahin/hello8920/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.appnucleus.abrarjahin.hello8920;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
