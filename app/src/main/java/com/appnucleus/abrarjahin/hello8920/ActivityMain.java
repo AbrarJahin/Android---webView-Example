b39655e8ee417a5c1c9a1c113e3b3c26c4b66cba
S. M. Abrar Jahin <abrarjahin@live.com>
2016-05-11 18:27:57 +0600

    Code review Fix - Ship tracker refresh

--- app/src/main/java/com/appnucleus/abrarjahin/hello8920/ActivityMain.java
+++ app/src/main/java/com/appnucleus/abrarjahin/hello8920/ActivityMain.java
@@ -1,13 +1,46 @@
 package com.appnucleus.abrarjahin.hello8920;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
 
-public class ActivityMain extends AppCompatActivity {
-
+public class ActivityMain extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if ( isNetworkAvailable() )     //check if internet available or not
+        {
+            Toast.makeText(
+                                ActivityMain.this,
+                                "Internet Connected",
+                                Toast.LENGTH_LONG
+                            ).show();
+            WebView webview = (WebView)findViewById(R.id.webView);
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.loadUrl(getString(R.string.sound_cloud_url));
+        }
+        else    //Not connected
+        {
+            Toast.makeText(
+                    ActivityMain.this,
+                    "Internet Disconnected",
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+    }
+
+    //<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+    //<uses-permission android:name="android.permission.INTERNET"/>
+    public boolean isNetworkAvailable()
+    {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
     }
 }
